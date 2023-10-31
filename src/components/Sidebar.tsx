15de470caba4f91d9a36a0f892f0c6c3746914a9
kenny <kennychan09870@gmail.com>
2023-10-31 15:18:39 -0700

    changed db url

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,8 +8,7 @@ import { useAtom } from "jotai";
 import { yearAtom, sectorOptionsAtom, quantilesAtom, employmentAtom, topSectorsMapAtom, selectedSectorAtom } from "../App";
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('https://keen-accurately-skink.ngrok-free.app');
-// const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://industry-atlas-app.pockethost.io');
 const startYear = 1975;
 const endYear = 2016;
 const options = Array.from({ length: endYear - startYear + 1 }, (_, index) => ({
